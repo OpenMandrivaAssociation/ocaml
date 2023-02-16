62545c41e4609eac795e95be383479eb36b2d06f
Bernhard Rosenkränzer <bero@lindev.ch>
2023-02-16 10:54:44 +0100

    Adapt check-update.sh to current website

--- check-update.sh
+++ check-update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl https://ocaml.org/releases 2>/dev/null |grep "/releases/" |head -n1 |sed -e 's,.*/releases/,,;s,".*,,'
+curl https://ocaml.org/releases 2>/dev/null |grep "/releases/[0-9\.]*\"" |head -n1 |sed -e 's,.*/releases/,,;s,".*,,'
